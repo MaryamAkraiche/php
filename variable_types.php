e05d703109c4c44693580e6e37a6d15d4cdb4ffd
Maryam Akraiche <akraiche.maryam@gmail.com>
2024-05-27 17:00:48 +0200

    added exercices of variable types

---
+++ variable_types.php
@@ -0,0 +1,19 @@
+<?php
+$first_name = "Maryam";
+$age = 22;
+$eyeColor = "brown";
+$family = array(
+    0 => 'Cindy', 
+    1 => 'Jean', 
+    2 => 'Mustapha', 
+    3 => 'Pierre', 
+    4=>'Eric');
+
+echo "<p>Hi! My name is $first_name.</p>";
+echo "<p>I am $age years old.</p>";
+echo "<p>My eyes are $eyeColor.</p>";
+echo "<p>The first person in my family is $family[0]";
+?>
+
+<!-- 4-we will use Arrays -->
+<!-- 6-to store the information whether you are hungry or not we use boolean variable -->
